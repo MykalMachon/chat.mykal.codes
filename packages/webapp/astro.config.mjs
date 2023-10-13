41ff3fc71bca4d04e955c53e83ac61fd0e47838e
Mykal Machon <mykal@tinybox.dev>
2023-10-13 16:53:44 -0700

    feat(webapp): migrating to node adapter

--- packages/webapp/astro.config.mjs
+++ packages/webapp/astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
