118a6da219942d71c082a582ed4a9759b1542e3c
Mykal Machon <mykal@tinybox.dev>
2023-10-13 21:10:08 -0700

    feat(webapp): basic styles

--- packages/webapp/src/components/App.svelte
+++ packages/webapp/src/components/App.svelte
@@ -1,65 +1,176 @@
 <script>
-	const API_BASE = "/api/chat"
-	
-	let question = ""
-	let answer = null;
-	let loading = false;
+  import IoMdSend from 'svelte-icons/io/IoMdSend.svelte';
+  const API_BASE = '/api/chat';
 
-	const submitQuestionForm = async (e) => {
-		e.preventDefault();
-		loading = true;
-		
-		// get data from form as object
-		const form = new FormData(e.target);
-		const formData = Object.fromEntries(form.entries())
+  let formEl = null;
+  let question = '';
+  let answer = null;
+  let loading = false;
 
-		// construct URL params 
-		const params = new URLSearchParams(formData)
+  const handleEnterKey = (e) => {
+    console.log(e.key);
+    if (e.key == 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      console.log('submitting form');
+      formEl.dispatchEvent(new Event('submit'));
+    }
+  };
 
-		// call the API 
-		const res = await fetch(`${API_BASE}?${params.toString()}`);
-		const data = await res.json();
+  const submitQuestionForm = async (e) => {
+    e.preventDefault();
+    loading = true;
 
-		answer = data;
-		loading = false;
-	}
+    try {
+      // get data from form as object
+      const form = new FormData(e.target);
+      const formData = Object.fromEntries(form.entries());
+
+      // construct URL params
+      const params = new URLSearchParams(formData);
+
+      // call the API
+      const res = await fetch(`${API_BASE}?${params.toString()}`);
+      const data = await res.json();
+
+      if (res.status !== 200) throw new Error(data.message);
+
+      answer = data;
+    } catch (err) {
+      console.log('error', err);
+      answer = { answer: err, sources: [] };
+    }
+    question = '';
+    loading = false;
+  };
 </script>
 
-<h1>Mykal.<code>ai</code> Chat</h1>
+<nav>
+  <h1>Mykal.ai</h1>
+</nav>
 
-<section>
-	{#if !answer}
-		{#if loading}
-			<p>
-				loading...
-			</p>
-		{:else}
-			<p>
-				Ask a question!
-			</p>
-		{/if}
-	{:else}
-		<p>
-			{answer.answer}
-		</p>
-		<p>
-			sources: 
-			{answer.sources}
-		</p>
-	{/if}
+<main>
+  {#if !answer}
+    {#if loading}
+      <p>loading...</p>
+    {:else}
+      <p>Ask a question!</p>
+    {/if}
+  {:else}
+    <p>
+      {answer.answer}
+    </p>
+    <p>
+      {#if answer.sources}
+        sources:
+        {answer.sources}
+      {/if}
+    </p>
+  {/if}
+</main>
+
+<section class="chatform">
+  <form
+    bind:this={formEl}
+    on:submit={submitQuestionForm}
+    method="GET"
+    target="https://chat-api-production-cb57.up.railway.app/api/chat/"
+  >
+    <textarea
+      on:keydown={handleEnterKey}
+      id="q"
+      name="q"
+      bind:value={question}
+    />
+    <button
+      class="icon"
+      type="submit"
+      disabled={question.length <= 0 || loading}
+    >
+      <IoMdSend />
+    </button>
+  </form>
 </section>
 
-<form on:submit={submitQuestionForm} method="GET" target="https://chat-api-production-cb57.up.railway.app/api/chat/">
-	<input type="text" id="q" name="q" bind:value={question} />
-	<button type="submit" disabled='{question.length <= 0 || loading}'>
-		{#if loading} 
-			loading...
-		{:else}
-			{#if question.length <= 0}
-				Type a question!
-			{:else}
-				Ask a question!
-			{/if}
-		{/if}
-	</button>
-</form>
+<style>
+  @import 'https://unpkg.com/open-props';
+
+  :global(*) {
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+
+  :global(html, body) {
+    padding: 0;
+    margin: 0;
+  }
+
+	main, nav {
+    bottom: var(--size-3);
+    padding: var(--size-3);
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+	} 
+
+	p {
+		font-size: var(--font-size-md);
+	}
+
+  section.chatform {
+    position: absolute;
+    bottom: var(--size-3);
+    padding: var(--size-3);
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  form {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    gap: var(--size-4);
+    border: 1px solid var(--gray-3);
+    border-radius: calc(var(--radius-2) + var(--radius-2));
+    padding: var(--size-2);
+    max-width: 800px;
+    width: 100%;
+		box-shadow: var(--shadow-2);
+    &:focus-within {
+      border-color: var(--blue-3);
+      outline: 2px solid var(--blue-4);
+      outline-offset: 3px;
+    }
+  }
+
+  :global(input, select, textarea) {
+    padding: var(--size-2);
+    font-size: var(--font-size-1);
+    border: none;
+    resize: none;
+    &:focus {
+      outline: none;
+    }
+  }
+
+  :global(textarea) {
+    min-height: 100px;
+  }
+  :global(button.icon) {
+    width: 40px;
+    height: 40px;
+    padding: var(--size-2);
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: var(--radius-round);
+    background: var(--gradient-2);
+    border: none;
+    color: white;
+    transition: all 0.3s var(--ease-out-2);
+    &:disabled {
+      opacity: 0.6;
+    }
+  }
+</style>
